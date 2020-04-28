07970a7957afa1b17c251cbce63b6b29ab805c2f
Lucas Farias <lucas_fnf@hotmail.com>
2020-04-28 11:41:40 -0300

    Add test for cases with missing colon on bracket operations

---
+++ test/nfa/test_nfa_no_colon.cpp
@@ -0,0 +1,15 @@
+#include <test_nfa_common.hpp>
+
+int main(int argc, char **argv) {
+	// Simple alternation with epsilon path
+	compiler::NFA nfa;
+	std::set<compiler::NFATransition> nfa_test;
+
+	testExpectException<compiler::NFA::ExpectedColonException>(nfa, "{a{abc}{def}}");
+	testEqualNFATransitions(nfa, nfa_test);
+
+	testExpectException<compiler::NFA::ExpectedColonException>(nfa, "{la}");
+	testEqualNFATransitions(nfa, nfa_test);
+
+	return 0;
+}
