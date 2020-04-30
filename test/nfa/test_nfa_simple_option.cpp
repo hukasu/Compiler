eba645746ac80851729cb3bfb806e52ab2ae3eff
Lucas Farias <lucas_fnf@hotmail.com>
2020-04-30 16:27:28 -0300

    Add test for option

---
+++ test/nfa/test_nfa_simple_option.cpp
@@ -0,0 +1,18 @@
+#include <test_nfa_common.hpp>
+
+int main(int argc, char **argv) {
+	// Simple alternation
+	compiler::NFA nfa;
+	std::set<compiler::NFATransition> nfa_test{
+		compiler::NFATransition {0, 1, '\0'},
+		compiler::NFATransition {1, 2, 'a'},
+		compiler::NFATransition {2, 3, 'b'},
+		compiler::NFATransition {3, 4, 'c'},
+		compiler::NFATransition {1, 4, '\0'},
+	};
+
+	testSuccessfulRegistration(nfa, "{o:abc}");
+	testEqualNFATransitions(nfa, nfa_test);
+
+	return 0;
+}
