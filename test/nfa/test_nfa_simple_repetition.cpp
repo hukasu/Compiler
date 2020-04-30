3d49bc3a35a13bb3cdf5428549379c68c6fef231
Lucas Farias <lucas_fnf@hotmail.com>
2020-04-30 16:41:21 -0300

    Add repetition test

---
+++ test/nfa/test_nfa_simple_repetition.cpp
@@ -0,0 +1,20 @@
+#include <test_nfa_common.hpp>
+
+int main(int argc, char **argv) {
+	// Simple alternation
+	compiler::NFA nfa;
+	std::set<compiler::NFATransition> nfa_test{
+		compiler::NFATransition {0, 1, '\0'},
+		compiler::NFATransition {1, 6, '\0'},
+		compiler::NFATransition {2, 3, 'a'},
+		compiler::NFATransition {3, 4, 'b'},
+		compiler::NFATransition {4, 5, 'c'},
+		compiler::NFATransition {5, 6, '\0'},
+		compiler::NFATransition {6, 2, '\0'},
+	};
+
+	testSuccessfulRegistration(nfa, "{r:abc}");
+	testEqualNFATransitions(nfa, nfa_test);
+
+	return 0;
+}
